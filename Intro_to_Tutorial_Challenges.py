5ef152fe34a1811c63ee8f3f3059c6418d400ecf
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2022-11-15 09:58:00 +0100

    Update and rename Intro to Tutorial Challenges to Intro_to_Tutorial_Challenges.py

---
+++ Intro_to_Tutorial_Challenges.py
@@ -0,0 +1,5 @@
+def introTutorial(V, arr):
+    return arr.index(V)
+#________CODE DESCRIPTION_____________
+#i broke under the deadline stress and decided to get a free exercise to reach the total, will still try to do a cooler one but it1s hard ;(
+#this code just returns the index of the value V in the array arr
