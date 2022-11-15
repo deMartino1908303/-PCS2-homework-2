cd3be0e0645c53bf03f1e002dc12093bbe0b812e
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2022-11-15 09:59:28 +0100

    Update and rename Insertion Sort - Part 2 to Insertion_Sort_Part_2.py

--- Insertion_Sort_Part_2.py
+++ Insertion_Sort_Part_2.py
@@ -1,17 +1,17 @@
 def insertionSort2(n, arr):
     for count1 in range (1,n):
----------------------------#count of the number to examine from 1 to n-1 
+#---------------------------#count of the number to examine from 1 to n-1 
         change = False
 
         for count2 in range (count1-1,-1,-1):
----------------------------# decreasing fro cicle to see if the number before are smaller. if it finds any, it saves the position whereit is
----------------------------#bigger of the one before and smaller than the one after
+#---------------------------# decreasing fro cicle to see if the number before are smaller. if it finds any, it saves the position whereit is
+#---------------------------#bigger of the one before and smaller than the one after
             if arr[count1] < arr[count2]:
                 position = count2
                 change = True
                 
         if change :
----------------------------# if it has any change to do it does them here
+#---------------------------# if it has any change to do it does them here
             tmp_inset = arr.pop(count1)
             arr.insert(position,tmp_inset)
         print(" ".join(map(str, arr)))
